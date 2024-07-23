892bbf383b6347e2fd7415e27bd8f983de195dcd
Hyun Don Moon <hyundonmoon@gmail.com>
2024-07-23 16:19:45 +0900

    implement mark-done feature

--- src/index.ts
+++ src/index.ts
@@ -204,4 +204,49 @@ program
     logTodos(toggledItems);
   });
 
+program
+  .command('mark-done')
+  .alias('md')
+  .description('Mark todo items as done')
+  .action(async () => {
+    const todos = readTodos();
+
+    if (todos.length === 0) {
+      console.log(chalk.yellow('No todos found.'));
+      return;
+    }
+
+    const options = todos.filter((todo) => !todo.done);
+
+    const markedTodos = await checkbox({
+      message: 'Which todo item status would you like to mark as done?',
+      choices: options.map((item) => ({
+        name: `[ ] ${item.todo}`,
+        value: item,
+      })),
+      pageSize: 12,
+      required: true,
+    });
+
+    const markedIdsMap = markedTodos.reduce(
+      (acc, curr) => {
+        acc[curr.id] = true;
+        return acc;
+      },
+      {} as Record<string, true>
+    );
+
+    const updatedTodos = todos.map((todo) => {
+      if (markedIdsMap[todo.id]) {
+        todo.done = true;
+      }
+
+      return todo;
+    });
+
+    writeTodos(updatedTodos);
+    console.log('Marked the following items as done:');
+    logTodos(markedTodos);
+  });
+
 program.parse();
