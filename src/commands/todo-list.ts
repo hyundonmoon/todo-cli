101bd0af61a8df7891bda3bc7f35bb94e4c2fe3f
Hyun Don Moon <hyundonmoon@gmail.com>
2024-07-25 16:03:51 +0900

    implement todo-list as a standalone executable

---
+++ src/commands/todo-list.ts
@@ -0,0 +1,60 @@
+#!/usr/bin/env node
+import { Command } from 'commander';
+import readTodos from '../helper/readTodos.js';
+import chalk from 'chalk';
+import logTodos from '../helper/logTodos.js';
+
+const program = new Command();
+
+program.option('-d, done', 'List only finished todos');
+program.option('-a, all', 'List all todos');
+
+program.parse();
+
+function listTodos(options: Record<string, boolean>) {
+  const todos = readTodos();
+
+  if (todos.length === 0) {
+    console.log(chalk.yellow('No todos found.'));
+    return;
+  }
+
+  const checked = chalk.green('[x]');
+  const unchecked = chalk.red('[ ]');
+
+  if (options.all) {
+    logTodos(todos);
+  } else if (options.done) {
+    let index = 0;
+    todos.forEach((item) => {
+      // skip remaining todos
+      if (!item.done) {
+        return;
+      }
+
+      index++;
+      console.log(`${index}. ${checked} ${item.todo}`);
+    });
+
+    if (index === 0) {
+      console.log(chalk.yellow('No finished todos found.'));
+    }
+  } else {
+    let index = 0;
+    todos.forEach((item) => {
+      // skip finished todos
+      if (item.done) {
+        return;
+      }
+
+      index++;
+      console.log(`${index}. ${unchecked} ${item.todo}`);
+    });
+
+    if (index === 0) {
+      console.log(chalk.yellow('No remaining todos.'));
+    }
+  }
+}
+
+listTodos(program.opts());
--- src/index.ts
+++ src/index.ts
@@ -10,58 +10,11 @@ import logTodos from './helper/logTodos.js';
 
 program.version('1.0.0').description('To-Do List');
 
-program.command('add <todo>', 'Add a new todo').executableDir('./commands');
-
 program
-  .command('list', { isDefault: true })
+  .command('add <todo>', 'Add a new todo')
+  .command('list', 'List your todos', { isDefault: true })
   .alias('ls')
-  .description('List your todos')
-  .option('-d, done', 'List only finished todos')
-  .option('-a, all', 'List all todos')
-  .action((options) => {
-    const todos = readTodos();
-    if (todos.length === 0) {
-      console.log(chalk.yellow('No todos found.'));
-      return;
-    }
-
-    const checked = chalk.green('[x]');
-    const unchecked = chalk.red('[ ]');
-
-    if (options.all) {
-      logTodos(todos);
-    } else if (options.done) {
-      let index = 0;
-      todos.forEach((item) => {
-        // skip remaining todos
-        if (!item.done) {
-          return;
-        }
-
-        index++;
-        console.log(`${index}. ${checked} ${item.todo}`);
-      });
-
-      if (index === 0) {
-        console.log(chalk.yellow('No finished todos found.'));
-      }
-    } else {
-      let index = 0;
-      todos.forEach((item) => {
-        // skip finished todos
-        if (item.done) {
-          return;
-        }
-
-        index++;
-        console.log(`${index}. ${unchecked} ${item.todo}`);
-      });
-
-      if (index === 0) {
-        console.log(chalk.yellow('No remaining todos.'));
-      }
-    }
-  });
+  .executableDir('./commands');
 
 program
   .command('remove')
