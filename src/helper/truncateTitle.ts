664689c192e9b75b7d7d32f186b45b622ecbcf25
Hyun Don Moon <hyundonmoon@gmail.com>
2024-07-30 18:02:50 +0900

    modify truncateTitle function to make it a little more reusable

--- src/helper/truncateTitle.ts
+++ src/helper/truncateTitle.ts
@@ -1,7 +1,7 @@
-export default function truncateTitle(title: string): string {
-  if (title.length <= 50) {
+export default function truncateTitle(title: string, maxLength = 50): string {
+  if (title.length <= maxLength) {
     return title;
   }
 
-  return `${title.slice(0, 47)}...`;
+  return `${title.slice(0, maxLength - 3)}...`;
 }
